67706cc6b890b544e50d370b1e071da5e6457699
root <huynda>
2024-07-11 11:25:02 +0700

    changed variables add example docker-compose

--- placement-api/scripts/entrypoint.sh
+++ placement-api/scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Populate the placement database"
     placement-manage db sync
 echo "Configure apache"
-    echo "ServerName " $HOST_IP >> /etc/apache2/apache2.conf
+    echo "ServerName " $CONTROLLER_IP >> /etc/apache2/apache2.conf
 echo "[i] Starting daemon..."
 # run apache httpd daemon
     rm -rf /var/run/apache2/*
